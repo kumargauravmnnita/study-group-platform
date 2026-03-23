118e452118eba1deb25660b5456b483b92b9b79c
Kumar Gaurav <266802710+kumargauravmnnita@users.noreply.github.com>
2026-03-23 13:16:53 +0530

    Updated User Model with Password Hashing

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -21,6 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Password is required"],
     minlength: [6, "Password must be at least 6 characters"],
+    select: false,
   },
   profilePicture: {
     type: String,
@@ -50,5 +52,21 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
